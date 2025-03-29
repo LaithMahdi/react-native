fd34b8b8880436ad4730262f17a67ef4c3dde678
LaithMahdi <mahdilaith380@gmail.com>
2025-03-29 11:08:53 +0100

    Update app configuration, add custom fonts, and enhance layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,10 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
+      <Text className="text-4xl  font-rubik-bold">Welcome</Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">explore</Link>
       <Link href="/profile">Profile</Link>
